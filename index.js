51584d7b12a827afd50c3a72b0f285c6c0906f00
Inkyz <crackteam.conf@gmail.com>
2024-07-27 14:21:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit({ auth: process.env.ACCESS_TOKEN });
 
 async function getStats() {
-  const username = 'InkyzProd;
+  const username = 'InkyzProd';
   const { data: repos } = await octokit.repos.listForUser({ username });
   const totalProjects = repos.length;
   const totalStars = repos.reduce((acc, repo) => acc + repo.stargazers_count, 0);
